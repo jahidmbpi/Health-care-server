c16e889ae559d0fd222ae1ab9371e3d83a7bfb29
Jahidul Islam <jahid.redmi10@gmail.com>
2025-10-26 02:09:31 +0600

    uodate

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,19 +1,23 @@
 import dotenv from "dotenv";
 dotenv.config();
 interface ENVConfig {
-  port: string;
+  PORT: string;
+  DATABASE_URL: string;
 }
 
-const LoadEnvVariable = () => {
-  const requiredVariable: string[] = ["PORT"];
+const LoadEnvVariable = (): ENVConfig => {
+  const requiredVariable: string[] = ["PORT", "DATABASE_URL"];
   requiredVariable.forEach((key) => {
-    if (process.env[key]) {
+    if (!process.env[key]) {
       throw new Error(`missing environmet variable ${key}`);
     }
   });
+
   return {
-    PORT: process.env.PORT,
+    PORT: process.env.PORT as string,
+    DATABASE_URL: process.env.DATABASE_URL as string,
   };
 };
 
 export const envVars = LoadEnvVariable();
+console.log(envVars);
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import { Server } from "http";
 import app from "./app";
-
+import { envVars } from "./app/config";
+import "dotenv/config";
 let server: Server;
 const PORT = 3000;
 const startServer = async () => {
   try {
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    app.listen(envVars.PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
