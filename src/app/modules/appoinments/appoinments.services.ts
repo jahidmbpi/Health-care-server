82b8e33a7656a76c8d46df6ddbda041bc96418b7
Jahidul Islam <jahid.redmi10@gmail.com>
2025-11-29 19:20:41 +0600

    update

--- src/app/modules/appoinments/appoinments.services.ts
+++ src/app/modules/appoinments/appoinments.services.ts
@@ -1,65 +1,75 @@
-import { Request } from "express";
 import { prisma } from "../../config/prisma";
 import AppError from "../../../helper/appError";
 import { JwtPayload } from "jsonwebtoken";
-import { uuidv4 } from "zod";
+import { v4 as uuidv4 } from "uuid";
 
 const createAppoinment = async (
   patient: JwtPayload,
   payload: { doctorId: string; scheduleId: string }
 ) => {
-  const isExsitPatient = await prisma.patient.findUnique({
-    where: {
-      email: patient.email,
-    },
-  });
-
-  if (!isExsitPatient) {
-    throw new AppError(404, "patient not found");
-  }
-
-  const doctorData = await prisma.doctor.findUnique({
-    where: {
-      id: payload.doctorId,
-    },
-  });
-  if (!doctorData) {
-    throw new AppError(404, "doctor not found");
-  }
-
-  const isExsistScedule = await prisma.doctorSchedule.findFirstOrThrow({
-    where: {
-      scheduleId: payload.scheduleId,
-      doctorId: doctorData.id,
-      isBooked: false,
-    },
-  });
-  const videoCallignId = JSON.stringify(uuidv4());
-
-  const appoinmentData = await prisma.appoinment.create({
-    data: {
-      patientId: isExsitPatient.id,
-      doctorId: doctorData.id,
-      scheduleId: payload.scheduleId,
-      videoCallingId: videoCallignId,
-      status: "SCHEDULED",
-      paymentStatus: "UNPAID",
-    },
-  });
-
-  const upadateDoctorSchedule = await prisma.doctorSchedule.update({
-    where: {
-      doctorId_scheduleId: {
-        doctorId: payload.doctorId,
-        scheduleId: payload.scheduleId,
+  const result = await prisma.$transaction(async (tnx) => {
+    const isExsitPatient = await prisma.patient.findUnique({
+      where: {
+        email: patient.email,
       },
-    },
-    data: {
-      isBooked: true,
-    },
+    });
+
+    if (!isExsitPatient) {
+      throw new AppError(404, "patient not found");
+    }
+
+    const doctorData = await prisma.doctor.findUnique({
+      where: {
+        id: payload.doctorId,
+      },
+    });
+    if (!doctorData) {
+      throw new AppError(404, "doctor not found");
+    }
+
+    const isExsistScedule = await prisma.doctorSchedule.findFirstOrThrow({
+      where: {
+        scheduleId: payload.scheduleId,
+        doctorId: doctorData.id,
+        isBooked: false,
+      },
+    });
+    const videoCallignId = uuidv4();
+
+    const appoinmentData = await prisma.appoinment.create({
+      data: {
+        patientId: isExsitPatient.id,
+        doctorId: doctorData.id,
+        scheduleId: payload.scheduleId,
+        videoCallingId: videoCallignId,
+        status: "SCHEDULED",
+        paymentStatus: "UNPAID",
+      },
+    });
+
+    const upadateDoctorSchedule = await prisma.doctorSchedule.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId: payload.doctorId,
+          scheduleId: payload.scheduleId,
+        },
+      },
+      data: {
+        isBooked: true,
+      },
+    });
+    const transactionId = uuidv4();
+    const paymentData = await prisma.payment.create({
+      data: {
+        appoinmentId: appoinmentData.id,
+        transectionId: transactionId,
+        amount: doctorData.appoinmentFee,
+      },
+    });
+    return appoinmentData;
   });
 
-  return appoinmentData;
+  return result;
 };
 
 export const appoinmentServices = {
